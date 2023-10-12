d8913ecbccd485a1d12ddef2108eb291e45b9c71
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2023-10-12 19:37:25 +0300

    add Student annd WeatherForecast classes

---
+++ DOTNET-WebAppMVC-Optional/Models/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace DOTNET_WebAppMVC_Optional.Models;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
